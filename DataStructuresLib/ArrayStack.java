8f692c3cd2d9ebb55746b49e1c490c07e3e7b02a
codyruff <cruff@students.stonehill.edu>
2024-10-13 19:31:32 -0400

    Finished ArrayStack methods

--- DataStructuresLib/ArrayStack.java
+++ DataStructuresLib/ArrayStack.java
@@ -1,2 +1,70 @@
-public class ArrayStack {
+package DataStructuresLib;
+
+public class ArrayStack<T> extends DataStructuresLib.MyStack {
+
+    T[] stack;
+    int size;
+
+    public ArrayStack(){
+        stack = new T [10];
+        size = 0;
+    }
+
+    public ArrayStack(int size){
+        stack = new T [size];
+        this.size = 0;
+    }
+
+    @Override
+    void push(Object value) {
+        size++;
+        if (size > stack.length){
+            T[] newStack = new T[stack.length * 2];
+            for (int i = 0; i < stack.length; i++) {
+                newStack[i] = stack[i];
+            }
+            stack = newStack;
+        } else {
+            stack[(size - 1)] = (T) value;
+        }
+    }
+
+    @Override
+    Object pop() {
+        T result = null;
+        if (size == 0) {
+            System.out.println("Removing from an empty stack");
+        } else {
+            result = stack[size - 1];
+            stack[size - 1] = null;
+        }
+        return result;
+    }
+
+    @Override
+    Object peek() {
+        if (size == 0){
+            System.out.println("Stack is Empty, Returned null");
+            return null;
+        } else {
+            return stack[size-1];
+        }
+
+    }
+
+    @Override
+    int size() {
+        return size;
+    }
+
+    @Override
+    boolean isEmpty() {
+        return (size == 0);
+    }
+
+    @Override
+    void clear() {
+        T[] newStack = new T[stack.length];
+        stack = newStack;
+    }
 }
